8e34e9d6dc6f26e39632905c055612faae6af831
~egoist <sahoosibaprasad2221@gmail.com>
2024-03-12 16:38:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function stringChop(str, size) {
   // your code here
+	  const numChunks = Math.ceil(str.length / size)
+  const chunks = new Array(numChunks)
+
+  for (let i = 0, o = 0; i < numChunks; ++i, o += size) {
+    chunks[i] = str.substr(o, size)
+  }
+
+  return chunks
 }
 
 // Do not change the code below
